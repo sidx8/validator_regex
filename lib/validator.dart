398c08ed38399a364e6996df7d8dce108e076c84
siddharth <siddharth0sinha@gmail.com>
2023-05-21 21:48:47 +0530

    package v1

---
+++ lib/validator.dart
@@ -0,0 +1,25 @@
+library validator;
+
+class Validator {
+  static bool email(String email) {
+    String emailRegEx =
+        r'^[a-zA-Z0-9.a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9.]{2,}$';
+    RegExp regExp = RegExp(emailRegEx);
+    return regExp.hasMatch(email);
+  }
+
+  static bool url(String url) {
+    String pattern = r'^(http|https):\/\/([\w.]+)+(:[0-9]{1,5})?(\/.*)?$';
+    RegExp regExp = RegExp(pattern);
+    return regExp.hasMatch(url);
+  }
+
+  static bool ipAddress(String ip) {
+    String pattern =
+        r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$' //IPv4
+        r'|' // OR
+        r'^((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))$'; //IPv6
+    RegExp regExp = RegExp(pattern);
+    return regExp.hasMatch(ip);
+  }
+}
---
+++ test/validator_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:validator/validator.dart';
+
+void main() {
+  test('Email Validation', () {
+    expect(Validator.email('test@test.com'), true);
+    expect(Validator.email('test@test'), false);
+    expect(Validator.email('test.com'), false);
+  });
+
+  test('URL Validation', () {
+    expect(Validator.url('https://www.test.com'), true);
+    expect(Validator.url('http://www.test.com'), true);
+    expect(Validator.url('www.test.com'), false);
+  });
+
+  test('IP Address Validation', () {
+    expect(Validator.ipAddress('192.168.1.1'), true); //IPv4
+    expect(Validator.ipAddress('255.255.255.255'), true); //IPv4
+    expect(Validator.ipAddress('255.255.255.256'), false); //IPv4
+    expect(Validator.ipAddress('2001:0db8:85a3:0000:0000:8a2e:0370:7334'),
+        true); //IPv6
+    expect(Validator.ipAddress('2001:0db8:85a3:0000:0000:8a2e:0370:733Z'),
+        false); //IPv6
+  });
+}
